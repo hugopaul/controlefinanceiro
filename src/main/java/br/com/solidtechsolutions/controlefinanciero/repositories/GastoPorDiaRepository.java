4a497dc1e31342c15e73efd9f2263280b2842387
hugopaul <hugo.4500@icloud.com>
2023-06-04 23:21:02 -0300

    update version

--- src/main/java/br/com/solidtechsolutions/controlefinanciero/repositories/GastoPorDiaRepository.java
+++ src/main/java/br/com/solidtechsolutions/controlefinanciero/repositories/GastoPorDiaRepository.java
@@ -23,15 +23,23 @@ public interface GastoPorDiaRepository extends JpaRepository<Gasto, String> {
             "ORDER BY dia_mes_ano ASC", nativeQuery = true)
     List<Gasto> buscarValorTotalUltimosMeses();
 
-    @Query(value = "SELECT 'Média diária ultimos 30 dias' as dia_mes_ano,\n" +
-            "AVG(valor) AS total_gasto\n" +
-            "FROM lancamento\n" +
-            "WHERE data_hora >= CURRENT_DATE - INTERVAL '30 days'", nativeQuery = true)
+    @Query(value = "SELECT 'Média diária ultimos 30 dias' AS dia_mes_ano,\n" +
+            "\t(sum(a.total_gasto)/COUNT(A.TOTAL_GASTO)) AS total_gasto \n" +
+            "FROM (SELECT TO_CHAR(data_hora, 'DD/MM') AS dia_mes_ano, SUM(valor) AS total_gasto\n" +
+            "\tFROM lancamento\n" +
+            "\tWHERE data_hora >= CURRENT_DATE - INTERVAL '30 days'\n" +
+            "\tGROUP BY dia_mes_ano\n" +
+            "\tORDER BY dia_mes_ano ASC\n" +
+            "  )AS A", nativeQuery = true)
     Gasto buscarMediaDiariaUltimos30Dias();
 
-    @Query(value = "SELECT 'Média mensal ultimos 12 meses' as dia_mes_ano,\n" +
-            "AVG(valor) AS total_gasto\n" +
-            "FROM lancamento\n" +
-            "WHERE data_hora >= CURRENT_DATE - INTERVAL '12 months'", nativeQuery = true)
+    @Query(value = "SELECT 'Média mensal ultimos 12 meses' as dia_mes_ano, \n" +
+            "\tSUM(A.TOTAL_GASTO)/(COUNT(A.TOTAL_GASTO)) AS TOTAL_GASTO \n" +
+            "FROM (SELECT TO_CHAR(data_hora, 'MM/YYYY') as dia_mes_ano, SUM(valor) as total_gasto\n" +
+            "    FROM lancamento\n" +
+            "    WHERE data_hora >= CURRENT_DATE - INTERVAL '12 months'\n" +
+            "    GROUP BY dia_mes_ano\n" +
+            "    ORDER BY dia_mes_ano ASC\n" +
+            "   ) AS A", nativeQuery = true)
     Gasto buscarMediaMensalUltimosMeses();
 }
